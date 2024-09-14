7ddd3ba84b7a58123905eacb73f914d5d3db8e2d
lucascogrossi <lucasmachado292@gmail.com>
2024-09-14 11:08:45 -0300

    add solutions

---
+++ Average of Levels in Binary Tree.cpp
@@ -0,0 +1,49 @@
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
+
+ class Solution {
+public:
+    vector<double> averageOfLevels(TreeNode* root) {
+        
+	queue<TreeNode*> q;
+    vector<double>answ;
+
+    if (root == nullptr)
+        return answ;
+
+	q.push(root);
+	int qLen;
+
+	while (!q.empty()){
+		qLen = q.size();
+        double sum = 0.0;
+
+		for (int i = 0; i < qLen; ++i) {
+			TreeNode *node = q.front();
+			q.pop();       
+            sum += node->val;
+    
+			if (node->left != nullptr)
+				q.push(node->left);
+			if (node->right != nullptr)
+				q.push(node->right);
+		    }
+            answ.push_back(sum / qLen); 
+	    }
+        return answ;
+        
+    }
+
+};
+
---
+++ Binary Tree Level Order Traversal.cpp
@@ -0,0 +1,48 @@
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
+
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        
+	queue<TreeNode*> q;
+    vector<vector<int>> answ;
+
+    if (root == nullptr)
+        return answ;
+	q.push(root);
+	int qLen;
+
+	while (!q.empty()){
+		qLen = q.size();
+        vector<int> level;
+
+		for (int i = 0; i < qLen; ++i) {
+			TreeNode *node = q.front();
+			q.pop();
+            level.push_back(node->val);
+            
+
+			if (node->left != nullptr)
+				q.push(node->left);
+			if (node->right != nullptr)
+				q.push(node->right);
+		    }
+            answ.push_back(level);
+	    }
+        return answ;
+        
+    }
+
+};
